0062fe6edfbfe8dfaa7db544337b397fc348d746
kulikovda.24 <demakulikov2006@gmail.com>
2022-11-24 11:49:53 +0300

    events consumer works

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,4 +1,12 @@
 package app;
 
-public class Application {
+import jdk.jfr.Event;
+
+import java.util.function.Consumer;
+
+public class Application implements Consumer<Event> {
+    @Override
+    public void accept(Event event) {
+
+    }
 }
