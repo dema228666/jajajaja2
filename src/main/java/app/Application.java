cab0d297a2ae69ef28785fca07791afda5466247
kulikovda.24 <demakulikov2006@gmail.com>
2022-12-01 11:30:33 +0300

    window title ready

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -2,6 +2,7 @@ package app;
 
 import io.github.humbleui.jwm.*;
 
+import java.io.File;
 import java.util.function.Consumer;
 
 /**
@@ -20,9 +21,19 @@ public class Application implements Consumer<Event> {
         // создаём окно
         window = App.makeWindow();
         // задаём обработчиком событий текущий объект
+        // задаём размер окна
+        window.setWindowSize(900, 900);
+        // задаём его положение
+        window.setWindowPosition(100, 100);
+        // задаём иконку
+        switch (Platform.CURRENT) {
+            case WINDOWS -> window.setIcon(new File("src/main/resources/windows.ico"));
+            case MACOS -> window.setIcon(new File("src/main/resources/macos.icns"));
+        }
         window.setEventListener(this);
         // делаем окно видимым
         window.setVisible(true);
+        window.setTitle("Java 2D");
     }
 
     /**
