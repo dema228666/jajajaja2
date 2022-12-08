1f7967bf1bfb4fd9be952c8b850d8469e121be9f
kulikovda.24 <demakulikov2006@gmail.com>
2022-12-08 11:49:18 +0300

    int cs

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -6,8 +6,8 @@ import io.github.humbleui.skija.Canvas;
 import io.github.humbleui.skija.Paint;
 import io.github.humbleui.skija.RRect;
 import io.github.humbleui.skija.Surface;
+import misc.CoordinateSystem2i;
 import misc.Misc;
-
 import java.io.File;
 import java.util.function.Consumer;
 
@@ -82,37 +82,38 @@ public class Application implements Consumer<Event> {
                 window.close();
             } else if (e instanceof EventFrameSkija ee) {
                 Surface s = ee.getSurface();
-                paint(s.getCanvas(), s.getWidth(), s.getHeight());
+                paint(s.getCanvas(), new CoordinateSystem2i(s.getWidth(), s.getHeight()));
             }
         }
 
-        /**
-         * Рисование
-         *
-         * @param canvas низкоуровневый инструмент рисования примитивов от Skija
-         * @param height высота окна
-         * @param width  ширина окна
-         */
-        public void paint(Canvas canvas, int height, int width) {
-            // запоминаем изменения (пока что там просто заливка цветом)
-            canvas.save();
-            // очищаем канвас
-            canvas.clear(APP_BACKGROUND_COLOR);
+    /**
+     * Рисование
+     *
+     * @param canvas   низкоуровневый инструмент рисования примитивов от Skija
+     * @param windowCS СК окна
+     */
+    public void paint(Canvas canvas, CoordinateSystem2i windowCS) {
+        // запоминаем изменения (пока что там просто заливка цветом)
+        canvas.save();
+        // очищаем канвас
+        canvas.clear(APP_BACKGROUND_COLOR);
 
-            // координаты левого верхнего края окна
-            int rX = width / 3;
-            int rY = height / 3;
-            // ширина и высота
-            int rWidth = width / 3;
-            int rHeight = height / 3;
-            // создаём кисть
-            Paint paint = new Paint();
-            // задаём цвет рисования
-            paint.setColor(Misc.getColor(100, 255, 255, 255));
-            // рисуем квадрат
-            canvas.drawRRect(RRect.makeXYWH(rX, rY, rWidth, rHeight, 4), paint);
+        // восстанавливаем состояние канваса
+        canvas.restore();
+        // координаты левого верхнего края окна
+        int rX = windowCS.getSize().x / 3;
+        int rY = windowCS.getSize().y / 3;
+        // ширина и высота
+        int rWidth =  windowCS.getSize().x  / 3;
+        int rHeight = windowCS.getSize().y  / 3;
+        // создаём кисть
+        Paint paint = new Paint();
+        // задаём цвет рисования
+        paint.setColor(Misc.getColor(100, 255, 255, 255));
+        // рисуем квадрат
+        canvas.drawRRect(RRect.makeXYWH(rX, rY, rWidth, rHeight, 4), paint);
 
-            // восстанавливаем состояние канваса
-            canvas.restore();
-        }
+        // восстанавливаем состояние канваса
+        canvas.restore();
+    }
     }
