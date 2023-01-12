2f41ee5695c5ecf09a3125e3cb5a1d68dff68756
kulikovda.24 <demakulikov2006@gmail.com>
2023-01-12 11:56:46 +0300

    adaptive layout

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -33,9 +33,16 @@ public class Application implements Consumer<Event> {
      */
     private final Label label;
     /**
+     * Первый заголовок
+     */
+    private final Label label2;
+    /**
+     * Первый заголовок
+     */
+    private final Label label3;
+    /**
      * радиус скругления элементов
      */
-
     public static final int C_RAD_IN_PX = 4;
     /**
      * Конструктор окна приложения
@@ -47,7 +54,15 @@ public class Application implements Consumer<Event> {
         window = App.makeWindow();
         // создаём первый заголовок
         label = new Label(window, true, PANEL_BACKGROUND_COLOR, PANEL_PADDING,
-                "Привет, мир!", true, true);
+                4, 4, 1, 1, 1, 1, "Привет, мир!", true, true);
+        // создаём второй заголовок
+        label2 = new Label(window, true, PANEL_BACKGROUND_COLOR, PANEL_PADDING,
+                4, 4, 0, 3, 1, 1, "Второй заголовок", true, true);
+
+        // создаём третий заголовок
+        label3 = new Label(window, true, PANEL_BACKGROUND_COLOR, PANEL_PADDING,
+                4, 4, 2, 0, 1, 1, "Это тоже заголовок", true, true);
+
         // задаём обработчиком событий текущий объект
         window.setEventListener(this);
         // создаём первый заголовок
@@ -117,8 +132,8 @@ public class Application implements Consumer<Event> {
         canvas.save();
         // очищаем канвас
         canvas.clear(APP_BACKGROUND_COLOR);
-        // рисуем заголовок в точке [100,100] с шириной и выостой 200
-        label.paint(canvas, new CoordinateSystem2i(100, 100, 200, 200));
+        // рисуем заголовок
+        label.paint(canvas, windowCS);
         // восстанавливаем состояние канваса
         canvas.restore();
     }
